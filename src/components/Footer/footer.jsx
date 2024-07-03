d81d4a155bea0faeb569ba79a70940c8638119be
AlexMqs <alexandremarques73000@gmail.com>
2024-07-03 23:46:13 +0200

    Ajout de mise en forme du footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,5 +1,15 @@
+import './footer.scss'
+import logo from '../../assets/KasaWhiteLogo.png'
+
 function Footer() {
-  return <div>Footer 🏡</div>
+   return (
+      <footer className="footer">
+         <div className="footer__logo">
+            <img src={logo} alt="Kasa Logo" />
+         </div>
+         <p className="footer__text">© 2020 Kasa. All rights reserved</p>
+      </footer>
+   )
 }
 
 export default Footer
