7408839a853787e53b03637ec9fecea63310936b
AlexMqs <alexandremarques73000@gmail.com>
2024-07-03 23:45:59 +0200

    Ajout de mise en forme du header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom'
+import './header.scss'
+import logo from '../../assets/KasaRedLogo.png'
+
 function Header() {
-   return <div>Header 🏡</div>
+   return (
+      <header className="header">
+         <div className="header__logo">
+            <img src={logo} alt="Kasa Logo" />
+         </div>
+         <nav className="header__nav">
+            <Link to="/" className="header__link">
+               Accueil
+            </Link>
+            <Link to="/A-Propos" className="header__link">
+               A Propos
+            </Link>
+         </nav>
+      </header>
+   )
 }
 
 export default Header
