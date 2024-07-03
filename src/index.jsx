b9365b0e37c544f5986cc2d1b0eab22ea2664ed2
AlexMqs <alexandremarques73000@gmail.com>
2024-07-03 23:49:47 +0200

    Mise en page de la page D'Accueil

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
+import './styles/stylesglobal.scss'
 import Home from './pages/Home/home'
 import HousingForm from './pages/Accommodation_sheet/Accommodationsheet'
 import About from './pages/About_page/Aboutpage'
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,4 +1,15 @@
+import Banner from '../../components/Banner/banner'
+import backgroundImage from '../../assets/Bannerhome.png'
+
 function Home() {
-   return <div>Page d'accueil 🏡</div>
+   return (
+      <div>
+         <Banner
+            backgroundImage={backgroundImage}
+            text="Chez vous, partout et ailleurs"
+            height="200px"
+         />
+      </div>
+   )
 }
 export default Home
