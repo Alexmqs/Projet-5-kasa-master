b0e15e622639b8c12d8f11e189c9c62a5bff857c
AlexMqs <alexandremarques73000@gmail.com>
2024-07-05 16:35:51 +0200

    Ajout de la banniere et des style pour la page a propos

--- src/pages/About_page/Aboutpage.jsx
+++ src/pages/About_page/Aboutpage.jsx
@@ -1,5 +1,17 @@
+import Banner from '../../components/Banner/banner'
+import backgroundImage from '../../assets/Bannerabout.png'
+import './aboutpage.scss'
+
 function About() {
-   return <div>A Propos 🏡</div>
+   return (
+      <div className="About__style">
+         <Banner
+            backgroundImage={backgroundImage}
+            text="Chez vous, partout et ailleurs"
+            height="200px"
+         />
+      </div>
+   )
 }
 
 export default About
