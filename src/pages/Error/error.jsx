0f71cc905da05c75c59fc6703175145872933692
AlexMqs <alexandremarques73000@gmail.com>
2024-07-11 16:04:18 +0200

    Ajout de la page d'erreur

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -1,5 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './error.scss'
+
 function Error() {
-  return <div>Page erreur 404 🏡</div>
+   return (
+      <div className="error-container">
+         <h1 className="error-code">404</h1>
+         <p className="error-message">
+            Oups! La page que vous demandez n'existe pas.
+         </p>
+         <Link to="/" className="error-link">
+            Retourner sur la page d'accueil
+         </Link>
+      </div>
+   )
 }
 
 export default Error
