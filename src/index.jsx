f6102ea9e0f16ec068786342001c976bb608d692
AlexMqs <alexandremarques73000@gmail.com>
2024-07-15 21:04:21 +0200

    Suppresion de ReactDOM pour passer en version react 18+

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles/stylesglobal.scss'
 import Home from './pages/Home/home'
 import HousingForm from './pages/Accommodation_sheet/accommodations'
@@ -9,7 +9,10 @@ import Error from './pages/Error/error'
 import Header from './components/Header/header'
 import Footer from './components/Footer/footer'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
    <React.StrictMode>
       <Router>
          <Header />
@@ -21,6 +24,5 @@ ReactDOM.render(
          </Routes>
          <Footer />
       </Router>
-   </React.StrictMode>,
-   document.getElementById('root')
+   </React.StrictMode>
 )
