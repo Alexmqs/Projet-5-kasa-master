f8cf24ca4362285ed7b1d2eca5d3ccb6dcfd2222
AlexMqs <alexandremarques73000@gmail.com>
2024-07-17 22:30:24 +0200

    Mise en place du carrousel

---
+++ src/components/Carrousel/carrousel.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+   faChevronLeft,
+   faChevronRight,
+} from '@fortawesome/free-solid-svg-icons'
+import './carrousel.scss' // Assurez-vous d'importer le fichier SCSS
+
+const Carousel = ({ pictures }) => {
+   const [currentIndex, setCurrentIndex] = useState(0)
+
+   const handlePrevClick = () => {
+      const newIndex =
+         currentIndex === 0 ? pictures.length - 1 : currentIndex - 1
+      setCurrentIndex(newIndex)
+   }
+
+   const handleNextClick = () => {
+      const newIndex =
+         currentIndex === pictures.length - 1 ? 0 : currentIndex + 1
+      setCurrentIndex(newIndex)
+   }
+
+   return (
+      <div className="carousel">
+         <button
+            className="carousel__button carousel__button--left"
+            onClick={handlePrevClick}
+         >
+            <FontAwesomeIcon icon={faChevronLeft} />
+         </button>
+         <img
+            className="carousel__image"
+            src={pictures[currentIndex]}
+            alt={`Slide ${currentIndex}`}
+         />
+         <div className="carousel__index">{`${currentIndex + 1}/${
+            pictures.length
+         }`}</div>
+         <button
+            className="carousel__button carousel__button--right"
+            onClick={handleNextClick}
+         >
+            <FontAwesomeIcon icon={faChevronRight} />
+         </button>
+      </div>
+   )
+}
+
+export default Carousel
--- src/pages/Accommodation_sheet/accommodations.jsx
+++ src/pages/Accommodation_sheet/accommodations.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import logements from '../../logements.json'
+import Carrousel from '../../components/Carrousel/carrousel'
+import Collapse from '../../components/Collapse/collapse'
+import './accommodations.scss'
 
 function HousingForm() {
    const { id } = useParams()
-   return <div>Fiche de logement 🏡 pour ID: {id}</div>
+   const navigate = useNavigate()
+   const accommodation = logements.find((logement) => logement.id === id)
+   const equipmentContent = accommodation.equipments.map((equipment, index) => (
+      <span key={index}>
+         {equipment}
+         <br />
+      </span>
+   ))
+   useEffect(() => {
+      if (!accommodation) {
+         navigate('/Erreur')
+      }
+   }, [accommodation, navigate])
+
+   if (!accommodation) {
+      return null
+   }
+
+   return (
+      <div className="accommodation__sheet">
+         <Carrousel pictures={accommodation.pictures} />
+         <h1>{accommodation.title}</h1>
+         <p>{accommodation.location}</p>
+         <div className="accommodation__host">
+            <img
+               src={accommodation.host.picture}
+               alt={`${accommodation.host.name} profile`}
+            />
+            <p>{accommodation.host.name}</p>
+         </div>
+         <p>{accommodation.tags}</p>
+         <p>{accommodation.rating}</p>
+
+         <Collapse
+            title="Description"
+            content={<p>{accommodation.description}</p>}
+         />
+         <Collapse title="Equipement" content={<p>{equipmentContent}</p>} />
+      </div>
+   )
 }
 
 export default HousingForm
